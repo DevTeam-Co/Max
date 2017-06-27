8f2f7b0bac009c7b3bb16ecc7ed635afce4b502a
DevTeam-Co <AwminYaghi85@gmail.com>
2017-06-27 17:40:27 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -76,13 +76,12 @@ function create_config( )
 		"msg-checks",
 		"plugins",
 	},
-    sudo_users = {311231963,sudo_id},
+    sudo_users = {274807882,sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
     info_text = [[
-@PartTeam
-By: @So8eil
+Testing :/
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
